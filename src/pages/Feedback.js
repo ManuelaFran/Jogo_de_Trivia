749980a17128388f89cca4cae87d013019b089a9
vinicius leandro <leandrocostavinicius@gmail.com>
2022-08-09 18:02:17 -0300

    Requisitos 18/19

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  componentDidMount() {
+    this.setInfoLocalStorage();
+  }
+
+  convertEmail = () => {
+    const { gravatarEmail } = this.props;
+    const email = md5(gravatarEmail).toString();
+    return email;
+  }
+
+  setInfoLocalStorage = () => {
+    const { name, score } = this.props;
+    const emailConverted = this.convertEmail();
+    const objRanking = {
+      name,
+      score,
+      picture: emailConverted,
+    };
+    if (!JSON.parse(localStorage.getItem('ranking'))) {
+      localStorage.setItem('ranking', JSON.stringify([]));
+    }
+    const readRanking = JSON.parse(localStorage.getItem('ranking'));
+    const newRanking = [...readRanking, objRanking].sort((a, b) => b.score - a.score);
+    localStorage.setItem('ranking', JSON.stringify(newRanking));
+  }
+
   render() {
     const { assertions, score, history } = this.props;
     const menorTres = <p data-testid="feedback-text">Could be better...</p>;
@@ -40,13 +67,16 @@ class Feedback extends Component {
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
   score: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
   history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
-
 };
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
   score: state.player.score,
+  name: state.player.name,
+  gravatarEmail: state.player.gravatarEmail,
 });
 
 export default connect(mapStateToProps)(Feedback);
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
       <div>
         <p data-testid="ranking-title">Ranking</p>
+        <div>
+          {
+            ranking.map((item, index) => (
+              <div key={ index }>
+                <img
+                  src={ item.gravatarEmail }
+                  alt={ item.name }
+                />
+                <p
+                  data-testid={ `player-name-${index}` }
+                >
+                  { item.name }
+                </p>
+                <p
+                  data-testid={ `player-score-${index}` }
+                >
+                  { item.score }
+                </p>
+              </div>
+            ))
+          }
+        </div>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+};
 
 export default Ranking;
